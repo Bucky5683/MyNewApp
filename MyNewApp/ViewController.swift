9bb19e652dab95b71eaf9de5d395249a2c555a49
Bucky <bucky5683@gmail.com>
2023-07-25 22:40:35 +0900

    Initial Commit

---
+++ MyNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNewApp
+//
+//  Created by 김서연 on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
